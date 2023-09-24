a5bacc5f89fe6b1125f2b42973ae5d814462ac75
test <test@gmail.com>
2023-09-24 23:05:54 +0300

    Подключил библиотеку parallax, прикрутил к header

--- app/js/app.js
+++ app/js/app.js
@@ -4,21 +4,33 @@
 		$('.scroll-down').on('click', function(){
 			$('html, body').animate({
 				scrollTop: $('.navbar').offset().top
-			}, 500, function(){
-				//console.log('callback function');
+			}, 500, function(){ 
+				//console.log('callback function'); 
 			});
 		});
 
 		$(window).on('scroll', function(e){
-
-			console.log(pageYOffset);
-			console.log($('.header').outerHeight(true));
-
 			if(pageYOffset > $('.header').outerHeight(true)){
 				$('.navbar').addClass('fixed');
 			} else {
 				$('.navbar').removeClass('fixed');
 			}
+
+			/*if($(window).outerWidth(true) < 1440 && $(window).outerHeight(true) < 769){
+				console.log($(window).outerWidth(true));
+				console.log($(window).outerHeight(true));
+				$('.header').css('background-position', 'center ' + ((pageYOffset / 3.7)) + 'px');
+			} else {
+				console.log($(window).outerWidth(true));
+				$('.header').css('background-position', 'center ' + ((pageYOffset / 3.7) + -85) + 'px');
+			}*/
 		});
+
+
+		$('.header').parallax({
+			imageSrc: 'images/dest/header_bg.jpg',
+			speed: .7,
+		});
+
 	});
 })(jQuery);
