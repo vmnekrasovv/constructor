0409c338cc3f64e2dea2d4236da8f5e843ced5db
test <test@gmail.com>
2023-09-24 21:33:05 +0300

    Добавил адаптив для header и navbar / добавил бургер / добавил скрипты для scroll down и фиксации navbar

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,24 @@
 (function($){
 	$(document).ready(function(){ 
 
+		$('.scroll-down').on('click', function(){
+			$('html, body').animate({
+				scrollTop: $('.navbar').offset().top
+			}, 500, function(){
+				//console.log('callback function');
+			});
+		});
+
+		$(window).on('scroll', function(e){
+
+			console.log(pageYOffset);
+			console.log($('.header').outerHeight(true));
+
+			if(pageYOffset > $('.header').outerHeight(true)){
+				$('.navbar').addClass('fixed');
+			} else {
+				$('.navbar').removeClass('fixed');
+			}
+		});
 	});
 })(jQuery);
